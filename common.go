6b9fdeae330a3f7b794aa9ee7663303ed38384a2
袁暘 <yuanyang@quantdo.com.cn>
2019-01-02 17:28:43 +0800

    新增通用函数

--- common.go
+++ common.go
@@ -1,5 +1,7 @@
 package autoorder
 
+import "math"
+
 // OrderID 本地报单编号
 type OrderID int64
 
@@ -13,7 +15,8 @@ const (
 	Sell
 )
 
-func (d Direction) opposite() Direction {
+// Opposite 获取买卖方向的反方向
+func (d Direction) Opposite() Direction {
 	switch d {
 	case Buy:
 		return Sell
@@ -22,4 +25,36 @@ func (d Direction) opposite() Direction {
 	default:
 		panic("Invalid direction.")
 	}
+}
+
+// Name 获取买卖方向的名字
+func (d Direction) Name() string {
+	switch d {
+	case Buy:
+		return "Buy"
+	case Sell:
+		return "Sell"
+	default:
+		panic("Invalid direction.")
+	}
+}
+
+// MaxFloat64 查找一组float64中的最大值
+func MaxFloat64(f ...float64) float64 {
+	var max float64
+	max = 0
+	for _, v := range f {
+		max = math.Max(max, v)
+	}
+	return max
+}
+
+// MinFloat64 查找一组float64中的最小值
+func MinFloat64(f ...float64) float64 {
+	var min float64
+	min = math.MaxFloat64
+	for _, v := range f {
+		min = math.Min(min, v)
+	}
+	return min
 }
