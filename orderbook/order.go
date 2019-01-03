a220a6d74411a48c78d23e3327696fff0ab5f39a
袁暘 <yuanyang@quantdo.com.cn>
2019-01-03 16:54:30 +0800

    精简代码结构

--- orderbook/order.go
+++ orderbook/order.go
@@ -4,34 +4,6 @@ import (
 	"gitlab.quantdo.cn/yuanyang/autoorder"
 )
 
-// type priceItem struct {
-// 	Price  float64
-// 	Volume int64
-// }
-
-type orderStatus byte
-
-const (
-	// OnTheWay 委托在途
-	OnTheWay orderStatus = 0x10
-	// Accepted 委托已接受
-	Accepted orderStatus = 0x20
-	// Rejected 委托已拒绝
-	Rejected orderStatus = 0x40
-	// Queue 队列中
-	Queue orderStatus = 1 << iota
-	// PartTrade 部分成交
-	PartTrade
-	// AllTrade 全部成交
-	AllTrade
-	// Cancel 撤销
-	Cancel
-)
-
-func (status orderStatus) ChangeStatus(s orderStatus) orderStatus {
-	return s
-}
-
 type order struct {
 	Volume       int64
 	TradedVolume int64
