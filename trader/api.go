d059b9a3d3a6dd241d54b585d5f091e2383ae63e
袁暘 <yuanyang@quantdo.com.cn>
2019-01-03 16:03:49 +0800

    新增交易API的Mock

--- trader/api.go
+++ trader/api.go
@@ -1,5 +1,41 @@
 package trader
 
+import (
+	"log"
+	"sync/atomic"
+
+	"gitlab.quantdo.cn/yuanyang/autoorder"
+)
+
 // TraderAPI autoorder通用报单接口
 type TraderAPI interface {
+	Order(d autoorder.Direction, price float64, vol int64) (autoorder.OrderID, error)
+	FAK(d autoorder.Direction, price float64, vol int64) (autoorder.OrderID, error)
+	Cancel(localID autoorder.OrderID) error
+}
+
+// MockTrader 测试Mock接口
+type MockTrader struct {
+	orderID int64
+}
+
+func (td *MockTrader) mockOrder(name string, d autoorder.Direction, price float64, vol int64) (autoorder.OrderID, error) {
+	log.Printf("MockTrader.%s called: %s %d@%f\n", name, d.Name(), vol, price)
+	return autoorder.OrderID(atomic.AddInt64(&td.orderID, 1)), nil
+}
+
+// Order 报单接口
+func (td *MockTrader) Order(d autoorder.Direction, price float64, vol int64) (autoorder.OrderID, error) {
+	return td.mockOrder("Order", d, price, vol)
+}
+
+// FAK FAK接口
+func (td *MockTrader) FAK(d autoorder.Direction, price float64, vol int64) (autoorder.OrderID, error) {
+	return td.mockOrder("FAK", d, price, vol)
+}
+
+// Cancel 撤单接口
+func (td *MockTrader) Cancel(localID autoorder.OrderID) error {
+	log.Printf("MockTrader.Cancel called with LocalID: %d\n", localID)
+	return nil
 }
