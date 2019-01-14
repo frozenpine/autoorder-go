5ca34d1e18fa10774a2eb9a50bd00346327b0db2
袁暘 <yuanyang@quantdo.com.cn>
2019-01-14 08:30:25 +0800

    修改函数名

--- utils.go
+++ utils.go
@@ -5,8 +5,8 @@ import "math"
 // Snapshot 数据快照
 type Snapshot map[string]interface{}
 
-// SnapshotMerge 合并两个快照至dst, 如存在Key重复, src的Value将覆盖dst的Value
-func SnapshotMerge(dst Snapshot, src Snapshot) Snapshot {
+// MergeSnapshot 合并两个快照至dst, 如存在Key重复, src的Value将覆盖dst的Value
+func MergeSnapshot(dst Snapshot, src Snapshot) Snapshot {
 	for key, value := range src {
 		dst[key] = value
 	}
