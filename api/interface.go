c413ae53906d4a4436cd9f28f359372bb1cac4d7
袁暘 <yuanyang@quantdo.com.cn>
2019-01-18 09:59:11 +0800

    新增连接API

--- api/interface.go
+++ api/interface.go
@@ -1,8 +1,15 @@
 package api
 
 import (
+	"time"
+
 	"gitlab.quantdo.cn/yuanyang/autoorder"
 )
+
+type connAPI interface {
+	Connect(addr string) error
+	Disconnect() error
+}
 
 type loginAPI interface {
 	Login(loginInfo map[string]string) error
@@ -11,14 +18,18 @@ type loginAPI interface {
 
 // TraderAPI autoorder通用报单接口
 type TraderAPI interface {
+	connAPI
 	loginAPI
 	Order(d autoorder.Direction, price float64, vol int64) (autoorder.OrderID, error)
 	FAK(d autoorder.Direction, price float64, vol int64) (autoorder.OrderID, error)
 	Cancel(localID autoorder.OrderID) error
+	QueryOrders(instrumentID string, from, to time.Time) error
+	QueryTrades() error
 }
 
 // MarketAPI autoorder通用行情接口
 type MarketAPI interface {
+	connAPI
 	loginAPI
 	Subscribe(ins ...string)
 	UnSubscribe(ins ...string)
