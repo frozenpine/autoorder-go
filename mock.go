f6a7dc05fcaae58f5d90d404aafa13664ebbc13d
袁暘 <yuanyang@quantdo.com.cn>
2019-01-24 12:37:11 +0800

    修正mock

--- mock.go
+++ mock.go
@@ -4,11 +4,20 @@ import (
 	"fmt"
 	"log"
 	"sync/atomic"
+	"time"
 )
 
 // MockTrader 测试Mock接口
 type MockTrader struct {
 	orderID int64
+}
+
+func (td *MockTrader) Connect(addr string) (err error) {
+	return
+}
+
+func (td *MockTrader) Disconnect() (err error) {
+	return
 }
 
 // Login 登录接口
@@ -50,4 +59,12 @@ func (td *MockTrader) FAK(d Direction, price float64, vol int64) (OrderID, error
 func (td *MockTrader) Cancel(localID OrderID) error {
 	log.Printf("MockTrader.Cancel called with LocalID: %d\n", localID)
 	return nil
+}
+
+func (td *MockTrader) QueryOrders(instrumentID string, from, to time.Time) (err error) {
+	return
+}
+
+func (td *MockTrader) QueryTrades() (err error) {
+	return
 }
